e9841d91295668c10148f11226a174de9e691172
Trevor L <trevorlong992@gmail.com>
2021-12-14 21:47:21 -0500

    Adding input search and main JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,24 @@
 var apiUrl = "https://api.coinlore.net/api/tickers/?start=200&limit=10";
-
-
+var coinData = "";
+var input = document.getElementById("cryptoInput");
+var cryptoText = document.getElementById("cryptoOutput");
 fetch(apiUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    var coinData = data.data
-    console.log(data);
-    for (var i = 0; i < coinData.length; i++) {
-      console.log(coinData[i].name);
-      console.log(coinData[i].price_usd);
-    }
+    coinData = data.data;
+    input.disabled = false;
   });
+
+  function searchFunction() {
+    var filter, txtValue;
+    filter = input.value.toUpperCase();
+    for (var i = 0; i < coinData.length; i++) {
+      if (filter == coinData[i].name.toUpperCase()) {
+        console.log(coinData[i].name);
+        console.log(coinData[i].price_usd);
+        cryptoText.textContent = coinData[i].name;
+      }
+    } 
+  }
